d6ee8e7cf0adcdb5081cdb76bde3a4521d5fc816
JMsou <asdgf6543210@gmail.com>
2024-09-14 14:57:28 +0800

    lesson5

--- lesson5/lesson5_3.py
+++ lesson5/lesson5_3.py
@@ -1,15 +1,16 @@
-import tools #module檔案要.py，這次module檔名叫tools
+# import tools #自訂的module檔案就是.py，這次module檔名叫tools
+import widget
 
 while True:
     height = 0
     weight = 0
-    cm, kg = tools.input_data() #呼叫子程式
+    cm, kg = widget.input_data() #呼叫子程式
 
     print(f'身高={cm}，體重={kg}')
-    BMI = tools.calculate_bmi(weight=kg,height=cm)
+    BMI = widget.calculate_bmi(weight=kg,height=cm)
     # 引數名稱呼叫 parameter = argument；優點可以不依照順序
     print(f'你的BMI值是{BMI}')
-    print(tools.get_status(BMI))
+    print(widget.get_status(BMI))
 
     play_again = input('還要繼續算BMI嗎？(y,n)')
     if play_again == 'n':
