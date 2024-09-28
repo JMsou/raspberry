414f0c962e0abcbfb9d6a2f037d6eebce83ff1a1
JMsou <asdgf6543210@gmail.com>
2024-09-28 11:02:43 +0800

    built-in temp sensor

--- pico/lesson6_7/mian1.py
+++ pico/lesson6_7/mian1.py
@@ -1,8 +1,13 @@
 import machine
 import time
 adc = machine.ADC(4)
+conversion_factor = 3.3/65535
 
 while True:  
-    temperature_value = adc.read_u16()
-    print(temperature_value)
-    time.sleep(3)
+    reading = adc.read_u16() * conversion_factor
+    # The temperature sensor measures the Vbe voltage of a biased bipolar diode, connected to the fifth ADC channel
+    # Typically, Vbe = 0.706V at 27 degrees C, with a slope of -1.721mV (0.001721) per degree. 
+    temperature = 27 - (reading - 0.706)/0.001721
+    print(f'{reading}V')
+    print(f'{temperature}C')
+    time.sleep(1)
